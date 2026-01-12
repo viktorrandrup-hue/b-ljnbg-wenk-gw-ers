553391bb89dcb96a432cb19d6b6fb696962eef23
viktorrandrup-hue <viktorrandrup@gmail.com>
2026-01-12 15:18:11 +0100

    sdfghj

---
+++ CaseWare Bruger Registrering.ps1
@@ -0,0 +1,16 @@
+﻿# Definer registreringsstivejen
+$CWRegPath = "HKCU:\SOFTWARE\CaseWare International\Working Papers\2024.00\Settings\CWLS"
+
+# Sikr at stien findes
+if (-not (Test-Path $CWRegPath)) {
+    New-Item -Path $CWRegPath -Force | Out-Null
+}
+
+# Hent brugernavn (uden domæne)
+$UserName = $env:USERNAME
+
+# Opret/overskriv værdierne
+New-ItemProperty -Path $CWRegPath -Name "Authorization" -PropertyType String -Value "7361472051E284426DB9" -Force
+New-ItemProperty -Path $CWRegPath -Name "Alert Frequency" -PropertyType DWord -Value 2 -Force
+New-ItemProperty -Path $CWRegPath -Name "Email" -PropertyType String -Value "$UserName@shlr.dk" -Force
+New-ItemProperty -Path $CWRegPath -Name "Name" -PropertyType String -Value $UserName -Force
